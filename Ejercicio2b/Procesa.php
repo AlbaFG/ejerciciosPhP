752b8c52ceb805ce47da59e1fe265425e6276fcd
alba <albafgfoto@gmail.com>
2016-09-29 12:22:49 +0200

    probando

---
+++ Ejercicio2b/Procesa.php
@@ -0,0 +1,45 @@
+<?php
+
+  //Variables del formulario
+  $superior=$_POST['superior'];
+  $inferior=$_POST['inferior'];
+  $comprobar=$_POST['comprobar'];
+ 
+  //Número aleatorio
+  $num_random=  rand($inferior, $superior); //Número aleatorio entre límites establecidos por el usuario
+  
+
+?>
+
+
+<html
+    <head>
+        <meta charmet="UTF-8">
+        <title>Numero aleatorio</title>
+    </head>
+    <body>
+        Número introducido: <?php echo $comprobar?><br><br>
+        Número a adivinar: <?php echo $num_random?><br>
+        
+           <?php
+           if ($comprobar==$num_random) //si el numero dado por el usuario es igual que el aleatorio
+               echo "Enhorabuena. Has acertado";
+           else
+           {
+               if($comprobar<$comprobar) //Si es menor
+               {
+                   echo "<br>Los numeros no coinciden."; 
+                   echo "<br>Tu número: $comprobar es menor que el generado $num_random";
+               }
+               else //Si es mayor
+               {
+                  echo "<br>Los números no coinciden. Inténtalo otra vez";
+                  echo "<br>Tu número: $comprobar es mayor que el generado $num_random";
+                  
+               }
+           }
+           
+           
+           ?>
+    </body>
+</html>
---
+++ Ejercicio2b/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+    <form action="Procesa.php" method="POST">
+        Limite inferior:
+        <input type="number" name="inferior"/><br><br>
+        Limite superior:
+        <input type="number" name="superior"/><br><br>
+        Numero comprobación:
+        <input type="number" name="comprobar"/><br><br>
+        <input type="submit" value="enviar" name="enviar"/>
+        
+    </form>
+        
+    </body>
+</html>
